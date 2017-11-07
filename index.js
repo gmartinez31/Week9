ca79b61ac750e34b23b06b304015e6dc985931dd
gmartinez31 <bhgustavo13@yahoo.com>
2017-11-07 10:12:50 -0600

    example chat

--- index.js
+++ index.js
@@ -14,9 +14,25 @@ app.get('/', function (req, resp) {
 
 io.on('connection', function(client) {
     console.log('CONNECTED');
+
+    client.on('incoming', function(msg) {
+        //io.emit broadcasts whatever to everyone
+        io.emit('chat-msg', msg);
+    });
+
+    client.on('join-room', function (room) {
+        client.join(room, function () {
+            console.log(client.rooms);
+            io.to(room).emit('chat-msg', '**new user joined**');
+        });
+        client.on('incoming', function (msg) {
+            io.to(msg.room).emit('chat-msg', msg.msg);
+        });
+    });
+    
     client.on('disconnect', function () {
         console.log('EXITED');
-    })
+    });
 })
 
 http.listen(8000, function () {
