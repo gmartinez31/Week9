15c782a951fc695e44e4d7a31fb4440ac5f780c4
gmartinez31 <bhgustavo13@yahoo.com>
2017-11-07 17:34:43 -0600

    basic chat app complete

--- chatapp/index.js
+++ chatapp/index.js
@@ -3,70 +3,24 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.set('view engine', 'hbs');
 app.use('/socket-io',
     express.static('node_modules/socket.io-client/dist'));
-
-app.get('/', function (req, resp) {
-    resp.render('layout.hbs');
+    
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-var numUsers = 0;
-
-io.on('connection', function (client) {
-    var addedUser = false;
-    console.log('CONNECTED');
-
-    // New Message shown to everyone //
-    client.on('incoming', function (msg) {
-        //io.emit broadcasts whatever to everyone
-        client.broadcast.emit('chat-msg', {
-            username: client.username,
-            message: msg
-        });
+io.on('connection', function (socket) {
+    console.log('USER CONNECTED');
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
     });
 
-    // Adding a new person to chat room //
-    client.on('add user', function (username) {
-        if (addedUser) return;
-        client.username = username;
-        ++numUsers;
-        addedUser = true;
-        client.emit('login', {
-            numUsers: numUsers
-        });
-        client.broadcast.emit('User Joined', {
-            username: client.username,
-            numUsers: numUsers
-        });
-    });
-
-    // When someone is typing //
-    client.on('typing', function () {
-        client.broadcast.emit('stop typing', {
-            username: client.username
-        });
-    });
-
-    // When someone stops typing //
-    client.on('stop typing', function () {
-        client.broadcast.emit('typing', {
-            username: client.username
-        });
-    });
-
-    // For disconnecting, this executes //
-    client.on('disconnect', function () {
-        if (addedUser) {
-            --numUsers;
-            client.broadcast.emit('User Left', {
-                username: client.username,
-                numUsers: numUsers
-            });
-        }
-        console.log('EXITED');
+    socket.on('disconnect', function () {
+        console.log('USER EXITED');
     });
 });
+
 
 
 http.listen(8000, function () {
